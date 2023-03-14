66fb85330fe31b949ffa95f0102966d50f8ad494
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2023-03-14 20:37:13 +0700

    fix: initialize allowedRoutes when request is not metrics path

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const Prometheus = require("prom-client");
 const ResponseTime = require("response-time");
 
 const {
-	requestCountGenerator,
-	requestDurationGenerator,
-	requestLengthGenerator,
-	responseLengthGenerator,
+  requestCountGenerator,
+  requestDurationGenerator,
+  requestLengthGenerator,
+  responseLengthGenerator,
 } = require("./metrics");
 
 const { normalizeStatusCode, normalizePath } = require("./normalizers");
@@ -16,170 +16,171 @@ const EXPRESS_PATH_PARAMETER_REGEX = /:\w+(?:_\w+)*/;
 const { extractRoutes } = require("./express-utils");
 
 const defaultOptions = {
-	metricsPath: "/metrics",
-	metricsApp: null,
-	authenticate: null,
-	collectDefaultMetrics: true,
-	collectGCMetrics: false,
-	collectAnyPaths: false, // opt-out
-	// buckets for response time from 0.05s to 2.5s
-	// these are arbitrary values since i dont know any better ¯\_(ツ)_/¯
-	requestDurationBuckets: Prometheus.exponentialBuckets(0.05, 1.75, 8),
-	requestLengthBuckets: [],
-	responseLengthBuckets: [],
-	extraMasks: [],
-	customLabels: [],
-	transformLabels: null,
-	normalizeStatus: true,
+  metricsPath: "/metrics",
+  metricsApp: null,
+  authenticate: null,
+  collectDefaultMetrics: true,
+  collectGCMetrics: false,
+  collectAnyPaths: false, // opt-out
+  // buckets for response time from 0.05s to 2.5s
+  // these are arbitrary values since i dont know any better ¯\_(ツ)_/¯
+  requestDurationBuckets: Prometheus.exponentialBuckets(0.05, 1.75, 8),
+  requestLengthBuckets: [],
+  responseLengthBuckets: [],
+  extraMasks: [],
+  customLabels: [],
+  transformLabels: null,
+  normalizeStatus: true,
 };
 
 function createExpressPrometheusExporterMiddleware(userOptions = {}) {
-	const options = { ...defaultOptions, ...userOptions };
-	const originalLabels = ["route", "method", "status"];
-	options.customLabels = new Set([...originalLabels, ...options.customLabels]);
-	options.customLabels = [...options.customLabels];
-	const { metricsPath, metricsApp, normalizeStatus } = options;
-	const allowedRoutes = [];
-	let isAllowedRoutesInitialized = false;
+  const options = { ...defaultOptions, ...userOptions };
+  const originalLabels = ["route", "method", "status"];
+  options.customLabels = new Set([...originalLabels, ...options.customLabels]);
+  options.customLabels = [...options.customLabels];
+  const { metricsPath, metricsApp, normalizeStatus } = options;
+  const allowedRoutes = [];
+  let isAllowedRoutesInitialized = false;
 
-	const app = express();
-	app.disable("x-powered-by");
+  const app = express();
+  app.disable("x-powered-by");
 
-	const requestDuration = requestDurationGenerator(
-		options.customLabels,
-		options.requestDurationBuckets,
-		options.prefix
-	);
-	const requestCount = requestCountGenerator(
-		options.customLabels,
-		options.prefix
-	);
-	const requestLength = requestLengthGenerator(
-		options.customLabels,
-		options.requestLengthBuckets,
-		options.prefix
-	);
-	const responseLength = responseLengthGenerator(
-		options.customLabels,
-		options.responseLengthBuckets,
-		options.prefix
-	);
+  const requestDuration = requestDurationGenerator(
+    options.customLabels,
+    options.requestDurationBuckets,
+    options.prefix
+  );
+  const requestCount = requestCountGenerator(
+    options.customLabels,
+    options.prefix
+  );
+  const requestLength = requestLengthGenerator(
+    options.customLabels,
+    options.requestLengthBuckets,
+    options.prefix
+  );
+  const responseLength = responseLengthGenerator(
+    options.customLabels,
+    options.responseLengthBuckets,
+    options.prefix
+  );
 
-	/**
-	 * Check whether incoming request path is actually registered on express.
-	 * Reduce the sample size, since you don't want to record any fools who hits random API.
-	 * Trust me bro, it makes your Prometheus Cries.
-	 */
-	const isPathAllowed = (method, path) => {
-		if (allowedRoutes.length === 0) return false;
-		return allowedRoutes.some(
-			(route) => route.method === method && route.path === path
-		);
-	};
+  /**
+   * Check whether incoming request path is actually registered on express.
+   * Reduce the sample size, since you don't want to record any fools who hits random API.
+   * Trust me bro, it makes your Prometheus Cries.
+   */
+  const isPathAllowed = (method, path) => {
+    if (allowedRoutes.length === 0) return false;
+    return allowedRoutes.some(
+      (route) => route.method === method && route.path === path
+    );
+  };
 
-	const initializeAllowedRoutes = (req) => {
-		const normalizedAllowedRoutes = extractRoutes(req.app).map((route) => ({
-			method: route.method,
-			path: normalizePath(route.path, [
-				...options.extraMasks,
-				EXPRESS_PATH_PARAMETER_REGEX,
-			]),
-		}));
+  const initializeAllowedRoutes = (req) => {
+    const normalizedAllowedRoutes = extractRoutes(req.app).map((route) => ({
+      method: route.method,
+      path: normalizePath(route.path, [
+        ...options.extraMasks,
+        EXPRESS_PATH_PARAMETER_REGEX,
+      ]),
+    }));
 
-		allowedRoutes.push(...normalizedAllowedRoutes);
+    allowedRoutes.push(...normalizedAllowedRoutes);
 
-		isAllowedRoutesInitialized = true
-	}
+    isAllowedRoutesInitialized = true;
+  };
 
-	/**
-	 * Corresponds to the R(equest rate), E(error rate), and D(uration of requests),
-	 * of the RED metrics.
-	 */
-	const recordResponse = ResponseTime((req, res, time) => {
-		const { originalUrl, method } = req;
-		// will replace ids from the route with `#val` placeholder this serves to
-		// measure the same routes, e.g., /image/id1, and /image/id2, will be
-		// treated as the same route
-		const path = normalizePath(originalUrl, options.extraMasks);
+  /**
+   * Corresponds to the R(equest rate), E(error rate), and D(uration of requests),
+   * of the RED metrics.
+   */
+  const recordResponse = ResponseTime((req, res, time) => {
+    const { originalUrl, method } = req;
 
-		if (!options.collectAnyPaths) {
-			// Initialize allowed routes
-			if (!isAllowedRoutesInitialized) initializeAllowedRoutes(req);
+    if (originalUrl === metricsPath) return;
 
-			// Route is not on the express stack.
-			if (!isPathAllowed(method, path)) return
-		}
+    // will replace ids from the route with `#val` placeholder this serves to
+    // measure the same routes, e.g., /image/id1, and /image/id2, will be
+    // treated as the same route
+    const path = normalizePath(originalUrl, options.extraMasks);
 
-		if (path !== metricsPath) {
-			const status = normalizeStatus
-				? normalizeStatusCode(res.statusCode)
-				: res.statusCode.toString();
+    if (!options.collectAnyPaths) {
+      // Initialize allowed routes
+      if (!isAllowedRoutesInitialized) initializeAllowedRoutes(req);
 
-			const labels = { route: path, method, status };
+      // Route is not on the express stack.
+      if (!isPathAllowed(method, path)) return;
+    }
 
-			if (typeof options.transformLabels === "function") {
-				options.transformLabels(labels, req, res);
-			}
-			requestCount.inc(labels);
+    const status = normalizeStatus
+      ? normalizeStatusCode(res.statusCode)
+      : res.statusCode.toString();
 
-			// observe normalizing to seconds
-			requestDuration.observe(labels, time / 1000);
+    const labels = { route: path, method, status };
 
-			// observe request length
-			if (options.requestLengthBuckets.length) {
-				const reqLength = req.get("Content-Length");
-				if (reqLength) {
-					requestLength.observe(labels, Number(reqLength));
-				}
-			}
+    if (typeof options.transformLabels === "function") {
+      options.transformLabels(labels, req, res);
+    }
+    requestCount.inc(labels);
 
-			// observe response length
-			if (options.responseLengthBuckets.length) {
-				const resLength = res.get("Content-Length");
-				if (resLength) {
-					responseLength.observe(labels, Number(resLength));
-				}
-			}
-		}
-	});
+    // observe normalizing to seconds
+    requestDuration.observe(labels, time / 1000);
 
-	if (options.collectDefaultMetrics) {
-		// when this file is required, we will start to collect automatically
-		// default metrics include common cpu and head usage metrics that can be
-		// used to calculate saturation of the service
-		Prometheus.collectDefaultMetrics({
-			prefix: options.prefix,
-		});
-	}
+    // observe request length
+    if (options.requestLengthBuckets.length) {
+      const reqLength = req.get("Content-Length");
+      if (reqLength) {
+        requestLength.observe(labels, Number(reqLength));
+      }
+    }
 
-	app.use(recordResponse);
+    // observe response length
+    if (options.responseLengthBuckets.length) {
+      const resLength = res.get("Content-Length");
+      if (resLength) {
+        responseLength.observe(labels, Number(resLength));
+      }
+    }
+  });
 
-	/**
-	 * Metrics route to be used by prometheus to scrape metrics
-	 */
-	const routeApp = metricsApp || app;
+  if (options.collectDefaultMetrics) {
+    // when this file is required, we will start to collect automatically
+    // default metrics include common cpu and head usage metrics that can be
+    // used to calculate saturation of the service
+    Prometheus.collectDefaultMetrics({
+      prefix: options.prefix,
+    });
+  }
 
-	routeApp.get(metricsPath, async (req, res, next) => {
-		if (typeof options.authenticate === "function") {
-			let result = null;
-			try {
-				result = await options.authenticate(req);
-			} catch (err) {
-				// treat errors as failures to authenticate
-			}
+  app.use(recordResponse);
 
-			// the requester failed to authenticate, then return next, so we don't
-			// hint at the existance of this route
-			if (!result) {
-				return next();
-			}
-		}
+  /**
+   * Metrics route to be used by prometheus to scrape metrics
+   */
+  const routeApp = metricsApp || app;
 
-		res.set("Content-Type", Prometheus.register.contentType);
-		return res.end(Prometheus.register.metrics());
-	});
+  routeApp.get(metricsPath, async (req, res, next) => {
+    if (typeof options.authenticate === "function") {
+      let result = null;
+      try {
+        result = await options.authenticate(req);
+      } catch (err) {
+        // treat errors as failures to authenticate
+      }
 
-	return app;
+      // the requester failed to authenticate, then return next, so we don't
+      // hint at the existance of this route
+      if (!result) {
+        return next();
+      }
+    }
+
+    res.set("Content-Type", Prometheus.register.contentType);
+    return res.end(await Prometheus.register.metrics());
+  });
+
+  return app;
 }
 
 module.exports = createExpressPrometheusExporterMiddleware;
